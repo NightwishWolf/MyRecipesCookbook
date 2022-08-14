303f4de8fe6801f774ee1b214763f43586e13f6a
NightwishWolf <60580397+NightwishWolf@users.noreply.github.com>
2022-08-14 13:25:13 +0200

    some cleanup

--- RecipeCookbook/Services/RecipeService.cs
+++ RecipeCookbook/Services/RecipeService.cs
@@ -16,7 +16,7 @@ namespace RecipeCookbook.Services
         public HttpClientHandler httpClientHandler = new HttpClientHandler();
         public RecipeService()
         {
-            //Make sure httpclient ignores invalid certicates
+            // httpclient ignores invalid certicates
             this.httpClientHandler.ClientCertificateOptions = ClientCertificateOption.Manual;
             this.httpClientHandler.ServerCertificateCustomValidationCallback =
                 (httpRequestMessage, cert, cetChain, policyErrors) =>
@@ -26,12 +26,10 @@ namespace RecipeCookbook.Services
         }
 
         //Getter function so everytime a call is run this is reinitialized
-        //That way we always grab the latest accesstoken from preferences
         public HttpClient client
         {
             get
             {
-
                 //Initialize httpclient based on handler
                 var client = new HttpClient(this.httpClientHandler);
 
@@ -42,11 +40,10 @@ namespace RecipeCookbook.Services
                 return client;
             }
         }
-
         public async Task<List<RecipeItem>> GetAllRecipes()
         {
-            //Call backend based on custom apiclient class
             HttpResponseMessage response;
+
             try
             {
                 response = await client.GetAsync("/Recipes");
@@ -55,53 +52,44 @@ namespace RecipeCookbook.Services
                 {
                     //Read content as normal string
                     var content = await response.Content.ReadAsStringAsync();
-                    //Because the response are teams in json format, convert those to actual team objects
+                    //Because the response are recipes in json format, convert those to actual recipe objects
                     return JsonConvert.DeserializeObject<List<RecipeItem>>(content);
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("Fuck");
+                Console.WriteLine("Getting recipes didn't work");
             }
-
-            //Check if successfull
-
 
             return null;
 
         }
-
-
-
         public async Task<RecipeItem> GetSingleRecipe(int itemId)
         {
-            //Call backend based on custom apiclient class
             HttpResponseMessage response;
+
             try
             {
                 response = await client.GetAsync("/Recipes/" + itemId);
 
                 if (response.IsSuccessStatusCode)
                 {
-                    //Read content as normal string
                     var content = await response.Content.ReadAsStringAsync();
-                    //Because the response are teams in json format, convert those to actual team objects
                     return JsonConvert.DeserializeObject<RecipeItem>(content);
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("Fuck");
+                Console.WriteLine("Getting single recipe didn't work");
             }
             return null;
         }
 
         public async Task<RecipeItem> PostRecipe (RecipeItem recipeItem)
         {
-            //Call backend based on custom apiclient class
             HttpResponseMessage response;
 
-                //Make a JSON from the user object
+                //Make a JSON from the recipe object
                 var json = JsonConvert.SerializeObject(recipeItem);
                 var content = new StringContent(json, Encoding.UTF8, "application/json");
 
@@ -111,21 +99,18 @@ namespace RecipeCookbook.Services
                 {
                     _ = Application.Current.MainPage.DisplayAlert("Failed", "Something went wrong", "Okay");
                 }
-           
 
-            //If it was successfull the response is transformed again, a pop-up for success appears and the content is returned
+            //If it was successfull the response is transformed again, and the content is returned
             var responseJson = await response.Content.ReadAsStringAsync();
             var responseContent = JsonConvert.DeserializeObject<RecipeItem>(responseJson);
-           // _ = Application.Current.MainPage.DisplayAlert("Succes!", "Recipe was added", "Okay");
             return responseContent;
         }
 
         public async Task<RecipeItem> EditRecipe(RecipeItem recipeItem)
         {
-            //Call backend based on custom apiclient class
             HttpResponseMessage response;
 
-            //Make a JSON from the user object
+            //Make a JSON from the recipe object
             var json = JsonConvert.SerializeObject(recipeItem);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
@@ -135,7 +120,6 @@ namespace RecipeCookbook.Services
             {
                 _ = Application.Current.MainPage.DisplayAlert("Failed", "Something went wrong", "Okay");
             }
-
 
             //If it was successfull the response is transformed again, a pop-up for success appears and the content is returned
             var responseJson = await response.Content.ReadAsStringAsync();
